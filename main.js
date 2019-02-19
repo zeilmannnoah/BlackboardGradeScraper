62fbd305973953e4e39adf43015561ed75382ec6
Noah Z <zeilmannnoah@gmail.com>
2019-02-19 00:17:31 -0600

    Completed scraper

---
+++ main.js
@@ -0,0 +1,149 @@
+const puppeteer = require('puppeteer'),
+    fs = require('fs');
+
+const USERNAME = process.env.BB_USERNAME,
+    PASSWORD = process.env.BB_PASSWORD;
+
+(async () => {
+    const browser = await puppeteer.launch({headless: true}),
+        page = await browser.newPage();
+
+    let courseData, courseFrame;
+
+    console.log('Loading CAS');
+    try {
+        await page.goto('https://blackboard.missouristate.edu/webapps/bb-auth-provider-cas-BB5a78bae342181/execute/casLogin?cmd=login&authProviderId=_102_1&redirectUrl=https%3A%2F%2Fblackboard.missouristate.edu%2Fwebapps%2Fportal%2Fexecute%2FdefaultTab');
+    }
+    catch (err) {
+        console.error('Error loading page', err);
+        return await browser.close();
+    }
+
+    console.log('Entering username and password');
+    try {
+        await page.type('input#username', USERNAME);
+        await page.type('input#password', PASSWORD);
+
+        await Promise.all([
+            page.waitForNavigation({waitUntil: 'domcontentloaded'}),
+            page.click('#BearPassForm > div > input')
+        ]);
+    }
+    catch (err) {
+        console.error('Error entering username and password into CAS.\t', err);
+        return await browser.close();
+    }
+
+    console.log('Loading Blackboard');
+    try {
+        await Promise.all([
+            page.waitForNavigation({waitUntil: 'domcontentloaded'}),
+            page.click('a[href^="/webapps/gradebook"]')
+        ]);
+    }
+    catch (err) {
+        console.error('Error loading Blackboard.\t', err);
+        return await browser.close();
+    }
+
+    console.log('Accepting cookies');
+    try {
+        await page.waitForSelector('#agree_button', {timeout: 10000});
+        await page.click('#agree_button');
+    }
+    catch (err) {
+        console.error('Error accepting cookies.\t', err);
+        return await browser.close();
+    }
+
+    console.log('Parsing Courses');
+    try {
+        courseFrame = page.frames().find(frame => frame.name() === 'mybbCanvas');
+
+        await courseFrame.waitForSelector('.stream_item', {timeout: 10000});
+
+        courseData = await courseFrame.evaluate((semesterCode) => {
+            let courses = document.querySelectorAll('.stream_item'),
+                courseData = [];
+
+            for (course of courses) {
+                let courseName = course.querySelector('.stream_area_name').innerText;
+                
+                if (courseName.toUpperCase().includes(semesterCode)) {
+                    let gradeValue = course.querySelector('.grade-value').innerText,
+                        lastUpdated = course.querySelector('.stream_datestamp').innerText === '' ? '-' : course.querySelector('.stream_datestamp').innerText;
+
+                    courseData.push({courseName, gradeValue, lastUpdated, gradeUrl: course.getAttribute('bb:rhs'), items: []});
+                }
+            }
+
+            return Promise.resolve(courseData);
+        }, getSemesterCode());
+    }
+    catch (err) {
+        console.error('Error parsing courses.\t', err);
+        return await browser.close();
+    }
+
+    console.log('Parsing Grades');
+    try {
+        const gradeFrame = courseFrame.childFrames().find(frame => frame.name() === 'right_stream_mygrades');
+
+        for (course of courseData) {
+            await Promise.all([
+                gradeFrame.goto('https://blackboard.missouristate.edu' + course.gradeUrl),
+                gradeFrame.waitForNavigation({waitUntil: 'domcontentloaded'})
+            ]);
+            
+            await gradeFrame.waitForSelector('#grades_wrapper', {timeout: 10000});
+
+            let parsedGrades = await gradeFrame.evaluate((course) => {
+                let gradedItems = document.querySelectorAll('.sortable_item_row:not(.calculatedRow)'),
+                    itemArr = [];
+
+                gradedItems.forEach(grade => {
+                    let title = grade.querySelector('.cell.gradable').innerText.split("\n")[0],
+                        due = grade.querySelector('.gradable > .activityType') ? grade.querySelector('.gradable > .activityType').innerText : 'No Due Date'
+                        type = grade.querySelector('.itemCat').innerText,
+                        submitted = grade.querySelector('.lastActivityDate').innerText === '' ? "Not Submitted" : grade.querySelector('.lastActivityDate').innerText,
+                        status = grade.querySelector('.timestamp > .activityType').innerText,
+                        isBoolean = grade.querySelector('.gradeStatus > span > span'),
+                        score = isBoolean ? isBoolean.innerText : grade.querySelector('.grade > .grade').innerText,
+                        total = isBoolean ? 'No Total' : grade.querySelector('.pointsPossible').innerText.replace('/', '');
+
+
+                    itemArr.push({title, due, type, submitted, status, score, total});
+                });
+
+                return Promise.resolve(itemArr);
+            }, course);
+
+            course.items = parsedGrades;
+            delete course.gradeUrl;
+        }
+
+    }
+    catch (err) {
+        console.error('Error parsing grades.\t', err);
+        return await browser.close();
+    }
+
+    try {
+        console.log(`Complete, ${courseData.length} courses scraped. Closing browser.`);
+        await browser.close();
+    }
+    catch (err) {
+        console.error('Error closing browser\t', err);
+        return;
+    }
+
+    fs.writeFileSync('courses.JSON', JSON.stringify(courseData, null, 4));
+})();
+
+function getSemesterCode() {
+    let today = new Date(),
+        month = today.getMonth(),
+        year = today.getFullYear().toString().substring(2);
+
+    return (month > 6 ? "FA" : "SP") + year;
+}
